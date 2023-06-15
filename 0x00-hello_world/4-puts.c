345dc5cf4fad8264b63e1f5f9d6d727ab14a6474
khaoula <khoula.ouarradi@gmail.com>
2023-06-15 14:35:24 -0700

    gcc

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is our entry point
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
