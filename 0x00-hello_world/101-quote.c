80a7c7a5e94a4508ccb3c6244fcf99fee9d1da48
khaoula <khoula.ouarradi@gmail.com>
2023-06-15 18:03:43 -0700

    gcc

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - starting point
+ *
+ * Return: Always 1 (success)
+*/
+int main(void)
+{
+	char jo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, jo, 59);
+	return (1);
+}
