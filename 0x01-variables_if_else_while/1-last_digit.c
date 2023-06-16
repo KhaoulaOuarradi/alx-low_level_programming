4fdcc2434dbb5010779a17e9823f56235f338d6b
khaoula <khoula.ouarradi@gmail.com>
2023-06-16 15:37:59 -0700

    gcc

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - entry point
  *
  * Description: print the value of n status:
- *              greater then, is zero, is not less than 6.
+ *		greater than, is zero and is not less than 6.
  *
  * Return: 0 (success)
 */
