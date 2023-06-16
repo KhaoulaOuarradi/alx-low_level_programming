d978cf2b484f40a1c53709addd0354aec778db75
khaoula <khoula.ouarradi@gmail.com>
2023-06-15 17:43:37 -0700

    gcc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  * Return: Always 0 (success)
 */
 int main(void)
-{	printf("size of a char: %u byte(s)\n", sizeof(char));
-	printf("size of an int: %u byte(s)\n", sizeof(int));
-	printf("size of along int: %u byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
-	printf("size of a flot: %u byte(s)\n", sizeof(float));
+{	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of along int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a flot: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
