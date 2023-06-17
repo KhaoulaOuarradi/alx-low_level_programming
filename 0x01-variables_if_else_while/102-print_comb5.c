3db3847bc2c4746c5eb965bc80846746b5982642
khaoula <khoula.ouarradi@gmail.com>
2023-06-16 19:36:37 -0700

    gcc

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ *Return: always (0)
+*/
+
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		int b =  48;
+
+		while (b < 58)
+		{
+			int c = a;
+
+			while (c < 58)
+			{
+				int d = 48;
+
+				if (a == c)
+					d = b + 1;
+				while (d < 58)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+					if ((a != 57) || (b != 56))
+					{
+						putchar(44);
+						putchar(32);
+					}
+				d++;
+				}
+			c++;
+			}
+		b++;
+		}
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
