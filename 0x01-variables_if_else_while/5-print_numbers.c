2ea39f4d5fdbb628cd42cd5fcbf9007bb95c1fab
khaoula <khoula.ouarradi@gmail.com>
2023-06-16 19:10:22 -0700

    gcc

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a++);
+	}
+	putchar('\n');
+	return (0);
+}
