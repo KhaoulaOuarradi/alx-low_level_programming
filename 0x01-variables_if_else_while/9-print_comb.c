206f61bf5c80ad90eeaa76d4f12edabb89fa1004
khaoula <khoula.ouarradi@gmail.com>
2023-06-16 19:19:54 -0700

    gcc

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
