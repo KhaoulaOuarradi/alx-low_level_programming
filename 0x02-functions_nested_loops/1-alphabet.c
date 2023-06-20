03ff2e210eb95de1730e723f343e673b6152978b
khaoula <khoula.ouarradi@gmail.com>
2023-06-20 11:50:41 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
  * print_alphabet(void)
  *
  * Return: This function returns void
-*/
+ */
 void print_alphabet(void)
 {
 	int index;
 
-	for (index =97; index < 123; index++)
+	for (index = 97; index < 123; index++)
 	{
 		_putchar(index);
 	}
