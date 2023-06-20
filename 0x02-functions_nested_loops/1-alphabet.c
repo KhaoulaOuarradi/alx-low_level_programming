8573e795c4ee0115b1731bc8e0eaf932d282300d
khaoula <khoula.ouarradi@gmail.com>
2023-06-20 11:41:44 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet(void)
+ *
+ * Return: This function returns void
+*/
+void print_alphabet(void);
+{
+	int index;
+
+	for (i =97; i < 123; index++);
+	{
+		_putchar(index);
+	}
+	_putchar('\n');
+}
