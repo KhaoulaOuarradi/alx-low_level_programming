48a952698b1d7c5725b65032fadbc3c27bf94a5e
khaoula <khoula.ouarradi@gmail.com>
2023-06-20 12:20:24 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - ten times
+ *
+ * Return: This functions return void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 97; j <= 122; i++)
+		}
+			_putchar(j);
+		}
+	_putchar('\n');
+	}
+}
