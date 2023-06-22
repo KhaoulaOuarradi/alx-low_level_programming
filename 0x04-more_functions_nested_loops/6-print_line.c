34c6dff83be8645f39b1e268b21c4c45a0b0ff3a
khaoula <khoula.ouarradi@gmail.com>
2023-06-22 13:49:14 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - drawing a straight line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i = 0;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
