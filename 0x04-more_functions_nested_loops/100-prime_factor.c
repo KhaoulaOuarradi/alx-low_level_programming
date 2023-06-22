02aeeacddd4780401a83af485fcf3c3663d19052
khaoula <khoula.ouarradi@gmail.com>
2023-06-22 14:52:08 -0700

    task11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - find and print the largest number
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	int i = 2;
+
+	while (i < n)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			continue;
+		}
+		i++;
+	}
+	printf("%d\n", i);
+	return (0);
+}
