55ac38663be938ade2af61a03103b47ad27c4b7a
khaoula <khoula.ouarradi@gmail.com>
2023-06-26 11:06:17 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - printing a string, followed by a new line
+ * @str: string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+}
