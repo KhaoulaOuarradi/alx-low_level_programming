fa130c671ffdbb1a1c44f2160ab719b906176a98
khaoula <khoula.ouarradi@gmail.com>
2023-06-29 16:12:34 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,17 +2,21 @@
 
 /**
  * *_strcat - concatenating two strings.
- * @*dest: first string
- * @*src: second string
+ * @*dest: pointer to destination
+ * @*src: pointer to source
  *
- * Return: a pointer to the resulting string 
+ * Return: pointer to the resulting string 
  */
 char *_strcat(char *dest, char *src)
 {
-	char dest[100] = "Hello, \0";
-	char src[] = "World!\0";
+	int c, c2;
 
-	strcat (dest, src);
+	c = 0;
+	while (dest[c])
+		c++;
 
-	printf("%s\n", dest);
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2];
+
+	return (dest);
 }
