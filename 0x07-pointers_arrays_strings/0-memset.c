bb9dee999295b2de89c77a0eaf8e1df30cd18ce5
khaoula <khoula.ouarradi@gmail.com>
2023-07-04 14:55:04 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills the fist n bytes
+ * @s: area
+ * @b: constant
+ * @n: number of bytes
+ *
+ * Return: pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
